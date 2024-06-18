6184e23ebc93374a6584da64983e8e73cb2b27b3
ankitadubey2004 <155213532+ankitadubey2004@users.noreply.github.com>
2024-06-18 16:43:16 +0530

    added

--- Num&Maths.js
+++ Num&Maths.js
@@ -26,4 +26,5 @@ console.log(Math.floor(4.9));
 console.log(Math.min(4,3,6,8,));
 console.log(Math.max(4,3,6,8));
 
-console.log(Math.random());
+console.log(Math.random()); //Isko hum jitne baar run karegein vo alag hi value dega between 0 and 1
+console.log(Math.floor(Math.random()*10)+1); //It means it gives min value 1 
